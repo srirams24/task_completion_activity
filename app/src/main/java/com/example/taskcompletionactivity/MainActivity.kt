e6a1b4c6781e0641931503ad60e08668711c7b97
Sriram Sekar <124426883+srirams24@users.noreply.github.com>
2024-11-26 06:17:56 +0000

    feat: Add, arrange and align the task status image, task completed and complimentary text vertically in the  TaskCompletionActivity composable

--- app/src/main/java/com/example/taskcompletionactivity/MainActivity.kt
+++ app/src/main/java/com/example/taskcompletionactivity/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.taskcompletionactivity.ui.theme.TaskCompletionActivityTheme
 
@@ -31,11 +37,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskManagerApp(modifier: Modifier = Modifier) {
-
+    TaskCompletionActivity()
 }
 
 @Composable
 fun TaskCompletionActivity(modifier: Modifier = Modifier) {
+    val statusImage = painterResource(R.drawable.ic_task_completed)
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = statusImage, contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.task_completion_text)
+        )
+        Text(
+            text = stringResource(R.string.compliment_text)
+        )
+    }
 
 }
 
